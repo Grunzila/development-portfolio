7b6528c2dc7ec1900fce2892d53ebe3ffd9f2926
Paul Roylance <grunzila@gmail.com>
2017-05-02 08:08:56 +0000

    added more models; wrote rough index.html; made more adjustments to make the template

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,3 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Blurb(models.Model):
+    blurb_text = models.CharField(max_length=200)
+    
+class Item(models.Model):
+    item_text = models.CharField(max_length=32)
+    pub_date = models.DateTimeField('date published')
+    
+class Visits(models.Model):
+    visits = models.IntegerField(default=0)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
 # Create your views here.
+
+from .models import Blurb
+
+class IndexView(generic.ListView):
+    template_name = 'portfolio/index.html'
+    model = Blurb
