6a4442186c858d88202e0a0b2a32049384ca9afc
Paul Roylance <grunzila@gmail.com>
2017-05-05 16:55:51 +0000

    added function for range and bits to delete children

--- portfolio/static/portfolio/script.js
+++ portfolio/static/portfolio/script.js
@@ -1,1 +1,11 @@
-var blurbs = document.body.querySelectorAll('.words');
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var parent = document.getElementsByClassName("blurb");
+var children = document.getElementsByClassName(".words");
+children.length;
+children[0] = 'Hangry';
+parent.removeChild.children[getRandomInt(0, children.length)];
