ad089deabee392830bd7c2efb1587c36de8057ea
Paul Roylance <grunzila@gmail.com>
2017-05-12 07:55:41 +0000

    adjusted javascript to more correct syntax

--- portfolio/static/portfolio/script.js
+++ portfolio/static/portfolio/script.js
@@ -4,8 +4,8 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-var parent = document.querySelectorAll("blurb");
-var children = document.getElementsByClassName(".words");
+var parent = document.querySelectorAll(".blurb");
+var children = document.querySelectorAll("#words");
 children.length;
 children[0] = 'Hangry';
-parent.removeChild.children[getRandomInt(0, children.length)];
+parent.removeChild(children[getRandomInt(0, children.length)]);
